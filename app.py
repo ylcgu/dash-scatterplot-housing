ffd735d8563f1d181a4a2a3b7b8ab3469136e2ee
Austin Lasseter <austinlasseter@gmail.com>
2019-08-23 17:23:42 -0400

    updated css stylesheet

--- app.py
+++ app.py
@@ -46,10 +46,10 @@ layout = go.Layout(
 fig = go.Figure(data=data, layout=layout)
 
 ########### Initiate the app
-app = dash.Dash()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 ########### Set up the layout
 app.layout = html.Div(children=[
